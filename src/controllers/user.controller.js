62a649f7bc1a265fa7b7bfb8e507791d5fb7a109
Timoideas <Timoideas@hotmail.com>
2021-08-09 09:26:35 -0500

    Token user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,59 +1,66 @@
+import { sign } from 'jsonwebtoken';
 import Validate from '../libraries/Validate';
 import UserSchema from '../models/user.schema';
 
 // GETTING all DB users
 export async function GET_users(req, res) {
-	try {
-		const users = await UserSchema.find();
-		res.status(200).json({ ok: true, data: users });
-	} catch ({ message }) {
-		res.status(200).json({ ok: false, message });
-	}
+  try {
+    const users = await UserSchema.find();
+    res.status(200).json({ ok: true, data: users });
+  } catch ({ message }) {
+    res.status(200).json({ ok: false, message });
+  }
 }
 
 // GETTING a DB user
 export async function GET_user(req, res) {
-	try {
-		const { id } = req.params;
-		const user = await UserSchema.findById(id);
-		res.status(200).json({ ok: true, data: user });
-	} catch ({ message }) {
-		res.status(200).json({ ok: false, message });
-	}
+  try {
+    const { id } = req.params;
+    const user = await UserSchema.findById(id);
+    res.status(200).json({ ok: true, data: user });
+  } catch ({ message }) {
+    res.status(200).json({ ok: false, message });
+  }
 }
 
 // CREATING a DB user
 export async function POST_user(req, res) {
-	try {
-		const body = await Validate(req.body, ['user']);
-		const user = new UserSchema(body);
-		const _user = await user.save();
-		res.status(200).json({ ok: true, data: _user });
-	} catch ({ message }) {
-		res.status(200).json({ ok: false, message });
-	}
+  try {
+    const body = await Validate(req.body, [
+      'username',
+      'name',
+      'telefono',
+      'password',
+      'correo',
+    ]);
+    const user = new UserSchema(body);
+    user.token = [sign(user, process.env.USER_SEED)];
+    const _user = await user.save();
+    res.status(200).json({ ok: true, data: { ..._user } });
+  } catch ({ message }) {
+    res.status(200).json({ ok: false, message });
+  }
 }
 
 // UPDATING a DB user
 export async function PUT_user(req, res) {
-	try {
-		const { id } = req.params;
-		const body = await Validate(req.body, ['user']);
-		const _user = await UserSchema.findOneAndUpdate({ _id: id }, body, {
-			new: true,
-		});
-		res.status(200).json({ ok: true, data: _user });
-	} catch ({ message }) {
-		res.status(200).json({ ok: false, message });
-	}
+  try {
+    const { id } = req.params;
+    const body = await Validate(req.body, ['compras', 'tokens', 'preferences']);
+    const _user = await UserSchema.findOneAndUpdate({ _id: id }, body, {
+      new: true,
+    });
+    res.status(200).json({ ok: true, data: _user });
+  } catch ({ message }) {
+    res.status(200).json({ ok: false, message });
+  }
 }
 
 // DELETING a DB user
 export async function DEL_user(req, res) {
-	const { id } = req.body;
-	UserSchema.findByIdAndDelete(id, (err, data) => {
-		if (err || !data) return res.status(200).json({ ok: false, incorrect: id });
-		res.status(200).json({ ok: true, data });
-	});
+  const { id } = req.body;
+  UserSchema.findByIdAndDelete(id, (err, data) => {
+    if (err || !data) return res.status(200).json({ ok: false, incorrect: id });
+    res.status(200).json({ ok: true, data });
+  });
 }
-
--- src/models/sale.schema.js
+++ src/models/sale.schema.js
@@ -1,8 +1,12 @@
 import { Schema, model } from 'mongoose';
 import validator from 'mongoose-unique-validator';
+import { Fecha } from '../libraries/Fecha';
 const SaleSchema = new Schema({
   sale: { type: String },
   owner: { type: Schema.Types.ObjectId, ref: 'User' },
+  total: { type: String },
+  details: { type: String },
+  timestamp: { type: Fecha },
 });
 SaleSchema.plugin(validator, { message: 'El {PATH} debería ser único' });
 export default model('Sale', SaleSchema);
